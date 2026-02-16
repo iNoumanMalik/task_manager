b11cd030387866942908476ccd6e3e4225b39884
Nouman Malik <inoumanmalik@outlook.com>
2026-02-16 17:29:35 +0500

    fixed typo mistake and changed bing to bind in main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from .database import engine,Base
 from .routes import router
 
-Base.metadata.create_all(bing=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task Management API")
 
